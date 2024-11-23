75dce0714ba941443a96f167064b4b5cbb06f588
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-11-23 23:13:58 +0100

    feat: added icons and images

---
+++ assets/index.ts
@@ -0,0 +1,23 @@
+//icons
+export { default as CompanyIcon } from "./icons/company-icon.svg";
+export { default as Contact } from "./icons/contact.svg";
+export { default as Dashboard } from "./icons/dashboard-icon.svg";
+export { default as Help } from "./icons/help.svg";
+export { default as Info } from "./icons/info.svg";
+export { default as JobIcon } from "./icons/job-icon.svg";
+export { default as JobTitle } from "./icons/job-title.svg";
+export { default as Location } from "./icons/location.svg";
+export { default as MenuICon } from "./icons/menu-icon.svg";
+export { default as Padlock } from "./icons/padlock.svg";
+export { default as ResumeIcon } from "./icons/resume-icon.svg";
+export { default as SearchIcon } from "./icons/search-icon.svg";
+export { default as UpArrow } from "./icons/up-arrow.svg";
+export { default as Bell } from "./icons/bell.svg";
+export { default as Grid } from "./icons/grid.svg";
+export { default as ArrowUp } from "./icons/arrow-up.svg";
+export { default as CalenderIcon } from "./icons/calender.svg";
+export { default as ArrowDown } from "./icons/down-arrow.svg";
+
+//images
+export { default as Logo } from "./images/logo.png";
+export { default as Userprofile } from "./images/user-avatar.png";
