a78663db4026685900bc4e2bd2b7f3e4d36a2763
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-11-24 20:12:00 +0100

    feat: added support for svg as a react component

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  images: {
+    domains: [""],
+  },
+  webpack: (config: any) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgo: false,
+            titleProp: true,
+            ref: true,
+          },
+        },
+        "url-loader",
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
