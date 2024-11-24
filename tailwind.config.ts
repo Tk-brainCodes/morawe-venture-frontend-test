30ff99de3fa850f90cc95e59a11b89b8562f5cac
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2024-11-24 20:12:50 +0100

    feat: configured colors and font styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,77 @@
 import type { Config } from "tailwindcss";
 
 export default {
-content: [
+    darkMode: ["class"],
+    content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  	extend: {
+  		colors: {
+			primary: '#5CE1E6',
+			gray: {
+				400: '#9CA3AF',
+				800: '#1F2A37',
+				900: '#111928',
+				50: '#F9FAFB'
+			},
+			customRed: {
+                100: '#FDE8E8',
+				800: '#9B1C1C'
+			},
+			customGreen: {
+               100: '#DEF7EC',
+			   800: '#03543F'
+			},
+			customBlue: {
+                600: '#1C64F2',
+				400: '#05264E'
+			},
+			customSlate: {
+                400: '#66789C',
+				100: '#A0ABB8'
+			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
